8ff2eb53abd9bd403614b20f0503836e489fe4fc
Fallout76isGOTY <156244964+Fallout76isGOTY@users.noreply.github.com>
2024-01-27 17:19:35 -0600

    Update images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -408,6 +408,102 @@ cccccccccccccccccccccccccccfffffffffffffffffffffffffffffffffffffffffffffffffffff
 cccccccccccccccccccccccccccfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffcccccccceeeeeeeeeeeeeeeeeeeeeeeeebbbbbbbbbddddd1d11
 ccccccccccccccccccccccccccffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffcccccccceeeeeeeeeeeeeeeeeeeebebebbbbbbbbbbddd111d11
 `;
+            case "image3":
+            case "ye2024":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . 6 6 6 6 6 6 6 6 . . . 
+. . . . 6 6 6 f 6 6 6 f 6 . . . 
+. . . . 6 6 6 6 6 6 6 6 6 6 . . 
+. . . . 6 6 6 6 6 f f f 6 6 . . 
+. . . . 6 6 6 6 f 6 6 6 f 6 . . 
+. . . . 6 6 6 6 f 6 f 6 f 6 . . 
+. . . . . 6 6 6 f f f f f . . . 
+. . . . . 1 1 1 1 1 1 . . . . . 
+. . . . f f f 1 1 f f . . . . . 
+. . . . e f f f f f f f f f 6 . 
+. . . . f f a f f a f . . . . . 
+. . . . 6 f f f f f f . . . . . 
+. . . . . f a f f a f . . . . . 
+. . . . . f f . . f f . . . . . 
+. . . . . 9 9 . . 9 9 . . . . . 
+`;
+            case "image4":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . a a a a a a a a . . . 
+. . . . a a a a a a a a 8 . . . 
+. . . . a a a a 8 8 8 8 8 8 . . 
+. . . . a a a 8 f 8 8 8 f 8 . . 
+. . . . a a 8 8 8 8 8 8 8 8 . . 
+. . . . a 8 8 8 8 f f f 8 8 . . 
+. . . . . f 8 8 8 8 8 8 8 . . . 
+. . . . . f f e e 1 f . . . . . 
+. . . . f f f 1 e 1 f f . . . . 
+. . . . f f f 1 e 1 f f . . . . 
+. . . . 8 f f f f f f 8 . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+. . . . . 6 6 . . 6 6 . . . . . 
+`;
+            case "image5":
+            case "myImage0":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 . . . 
+. . . . 1 1 1 1 1 1 1 1 4 . . . 
+. . . . 1 1 1 1 4 4 4 4 4 4 . . 
+. . . . 1 1 1 4 f 4 4 4 f 4 . . 
+. . . . 1 1 4 4 4 4 4 4 4 4 . . 
+. . . . 1 4 4 4 4 f f f 4 4 . . 
+. . . . . f 4 4 4 4 4 4 4 . . . 
+. . . . . f f e e f f . . . . . 
+. . . . f f f 1 e 1 f f . . . . 
+. . . . f f f 1 e 1 f f . . . . 
+. . . . 4 f f f f f f 4 . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+. . . . . 6 6 . . 6 6 . . . . . 
+`;
+            case "image6":
+            case "myImage1":return img`
+. . . . . . 4 4 4 4 4 4 4 . . . 
+. . . . . 2 2 2 2 2 2 2 2 2 2 . 
+. . . . 2 4 2 4 f 2 4 2 4 f 2 . 
+. . . . 4 4 4 2 2 4 4 4 2 2 . . 
+. . . . 4 4 4 4 4 6 6 6 4 4 . . 
+. . . . 4 4 4 4 6 4 4 4 6 4 . . 
+. . . . 4 4 4 4 6 4 6 4 6 4 . . 
+. . . . . a 4 4 6 6 6 6 6 . . . 
+. . . . . a a a 1 1 a . . . . . 
+. . . . a a a a a 1 a a . . . . 
+. . . . a a a a a 1 a a . . . . 
+. . . . d a a a a 1 a d . . . . 
+. . . . . a a a a 1 a . . . . . 
+. . . . . a a a a 1 a . . . . . 
+. . . . . a a . . a a . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image7":
+            case "myImage2":return img`
+. . . . . a a a a . . . . . . . 
+. . . . . a a a f . . . . . . . 
+. . . . e a a 4 4 . . . . . . . 
+. . . . e a 4 4 4 . . . . . . . 
+. . . . e 6 6 6 6 . . . . . . . 
+. . . . e 6 6 6 6 6 6 6 4 . . . 
+. . . . e e e e e e e 6 4 . . . 
+. . . . e 6 6 6 6 . . e . . . . 
+. . f f f f f 6 6 . . . e . . . 
+. f . . 3 6 6 f 6 . . . e . . . 
+f . 3 . 3 6 3 6 f 8 8 8 e 8 8 f 
+f . . 3 3 3 6 6 f 8 8 8 e 8 8 f 
+f 3 3 3 2 3 3 3 f e e e e e e . 
+f . . 3 3 3 . . f . . . f f . e 
+f . 3 . 3 . 3 . f . . f 2 . f . 
+. f . . 3 . . f . . . f . . f . 
+. . f f f f f . . . . . f f . . 
+`;
         }
         return null;
     })
